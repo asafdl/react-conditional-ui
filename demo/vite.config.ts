6cff4631bf5fe0e697623ee73da8ce4055641b3e
asafdl <asafd@elementor.com>
2026-03-30 19:04:42 +0300

    ci: gh pages

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+    base: "/react-conditional-ui/",
     plugins: [react()],
     resolve: {
         alias: {
